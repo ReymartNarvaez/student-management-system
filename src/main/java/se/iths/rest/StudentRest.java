1da8868c1ea341fd8f3ca86882c882323ee744ec
Reymart Narvaez <reymart_narvaez@hotmail.com>
2022-02-21 10:26:39 +0100

    Added CRUD methods in StudentRest

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,7 +1,47 @@
 package se.iths.rest;
 
+import se.iths.entity.Student;
+import se.iths.service.StudentService;
 
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("student")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class StudentRest {
 
+    @Inject
+    StudentService studentService;
 
+    @Path("new")
+    @POST
+    public Response createStudent(Student student) {
+        studentService.createStudent(student);
+        return Response.ok(student).build();
+    }
+
+    @Path("getall")
+    @GET
+    public Response getAll() {
+        List<Student> foundStudents = studentService.getAllStudent();
+        return Response.ok(foundStudents).build();
+    }
+
+    @Path("id")
+    @PUT
+    public Response updateLastName(Student student) {
+        studentService.updateLastName(student);
+        return Response.ok(student).build();
+    }
+
+    @Path("{id}")
+    @DELETE
+    public Response deleteStudent(@PathParam("id") Long id) {
+        studentService.deleteStudent(id);
+        return Response.ok().build();
+    }
 }
