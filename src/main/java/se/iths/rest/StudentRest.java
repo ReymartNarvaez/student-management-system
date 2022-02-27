582d2478888b8a2483ab412efba51649e2cf6fe9
Reymart Narvaez <reymart_narvaez@hotmail.com>
2022-02-27 10:51:43 +0100

    Added Exceptions in StudentRest

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,6 +1,8 @@
 package se.iths.rest;
 
 import se.iths.entity.Student;
+import se.iths.exception.BadRequestException;
+import se.iths.exception.NotFoundException;
 import se.iths.service.StudentService;
 
 import javax.inject.Inject;
@@ -20,7 +22,11 @@ public class StudentRest {
     @Path("new")
     @POST
     public Response createStudent(Student student) {
-        studentService.createStudent(student);
+        try {
+            studentService.createStudent(student);
+        } catch (Exception e) {
+            throw new BadRequestException("All Fields Required Except Phone Number");
+        }
         return Response.ok(student).build();
     }
 
@@ -28,6 +34,10 @@ public class StudentRest {
     @GET
     public Response getAll() {
         List<Student> foundStudents = studentService.getAllStudent();
+
+        if (foundStudents.isEmpty()){
+            throw new NotFoundException("No Students Found");
+        }
         return Response.ok(foundStudents).build();
     }
 
@@ -35,20 +45,33 @@ public class StudentRest {
     @GET
     public Response getStudent(@QueryParam("name") String name) {
         List<Student> foundStudents = studentService.findStudent(name);
+
+        if (foundStudents.isEmpty()) {
+            throw new NotFoundException("Student Does Not Exist");
+        }
         return Response.ok(foundStudents).build();
     }
 
     @Path("updatelastname/{id}")
     @PATCH
     public Response updateLastName(@PathParam("id") Long id, @QueryParam("lastName") String lastName) {
-        Student updateLastName = studentService.updateLastName(id, lastName);
+        Student updateLastName = null;
+        try {
+            updateLastName = studentService.updateLastName(id, lastName);
+        } catch (Exception e) {
+           throw new BadRequestException("Student Id Does Not Exist");
+        }
         return Response.ok(updateLastName).build();
     }
 
     @Path("{id}")
     @DELETE
     public Response deleteStudent(@PathParam("id") Long id) {
-        studentService.deleteStudent(id);
+        try {
+            studentService.deleteStudent(id);
+        } catch (Exception e) {
+            throw new BadRequestException("Delete Failed, Student Id Does Not Exist");
+        }
         return Response.ok().build();
     }
 }
