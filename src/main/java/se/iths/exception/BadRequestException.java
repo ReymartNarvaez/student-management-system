478a2411284871d284d011dd1e07eff63b667b57
Reymart Narvaez <reymart_narvaez@hotmail.com>
2022-02-27 10:49:38 +0100

    Created Exceptions

---
+++ src/main/java/se/iths/exception/BadRequestException.java
@@ -0,0 +1,13 @@
+package se.iths.exception;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
+public class BadRequestException extends WebApplicationException {
+
+    public BadRequestException(String message) {
+        super(Response.status(Response.Status.BAD_REQUEST)
+                .entity(message)
+                .build());
+    }
+}
