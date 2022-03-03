710eaa16561fa74fe8a2b2a5784adc0c50f6532e
ReymartNarvaez <Reymart_narvaez@hotmail.com>
2022-03-03 10:49:04 +0100

    Added getter and setter in Subject Class

--- src/main/java/se/iths/entity/Subject.java
+++ src/main/java/se/iths/entity/Subject.java
@@ -1,4 +1,33 @@
 package se.iths.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
 public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotEmpty
+    private String Subject;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSubject() {
+        return Subject;
+    }
+
+    public void setSubject(String subject) {
+        Subject = subject;
+    }
 }
